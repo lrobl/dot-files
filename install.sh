f98134ee154b0c80b53bfa8ece2b68679ff26925
Rob Lawyer <lroblawyer@gmail.com>
2022-02-27 23:00:36 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "install lrobl vscode settings"
+
+mv ./settings.json $HOME/.config/Code/User/settings.json
+mv ./keybindings.json $HOME/.config/Code/User/keybindings.json
+mv ./extensions.json $HOME/.config/Code/User/extensions.json
+
+
