a0fc5301bf150141d11eab89b407a4ecca55dc6a
Rob Lawyer <lroblawyer@gmail.com>
2022-02-27 23:12:53 +0000

    added mkdir

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 echo "install lrobl vscode settings"
 echo $HOME
+
+mkdir $HOME/.config/Code
+mkdir $HOME/.config/Code/User
 mv $HOME/.dotfiles/settings.json $HOME/.config/Code/User/settings.json
 mv $HOME/.dotfiles/keybindings.json $HOME/.config/Code/User/keybindings.json
 mv $HOME/.dotfiles/extensions.json $HOME/.config/Code/User/extensions.json
