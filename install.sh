6d0f6cf92ab06b9871f0fec13e20be3e800ef52b
Rob Lawyer <lroblawyer@gmail.com>
2022-02-27 23:09:06 +0000

    Edit location of settings file

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 echo "install lrobl vscode settings"
 echo $HOME
-mv $HOME/dotfiles/settings.json $HOME/.config/Code/User/settings.json
-mv $HOME/dotfiles/keybindings.json $HOME/.config/Code/User/keybindings.json
-mv $HOME/dotfiles/extensions.json $HOME/.config/Code/User/extensions.json
+mv $HOME/.dotfiles/settings.json $HOME/.config/Code/User/settings.json
+mv $HOME/.dotfiles/keybindings.json $HOME/.config/Code/User/keybindings.json
+mv $HOME/.dotfiles/extensions.json $HOME/.config/Code/User/extensions.json
 
 
