ed1ca02978e9550549f7f9489aee30d05ec1e45e
Rob Lawyer <37316368+lrobl@users.noreply.github.com>
2022-02-28 18:14:51 -0700

    add eslint file, and move the entire viscose folder in one go

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 echo "install lrobl vscode settings"
 
 echo "Copying dotfiles to: {$GITPOD_REPO_ROOT}"
-mkdir $GITPOD_REPO_ROOT/.vscode
-mv $HOME/.dotfiles/settings.json $GITPOD_REPO_ROOT/.vscode
-mv $HOME/.dotfiles/keybindings.json $GITPOD_REPO_ROOT/.vscode
-mv $HOME/.dotfiles/extensions.json $GITPOD_REPO_ROOT/.vscode
+# mkdir $GITPOD_REPO_ROOT/.vscode
+mv $HOME/.dotfiles/.vscode $GITPOD_REPO_ROOT/
+mv $HOME/.dotfiles/.eslintrc
