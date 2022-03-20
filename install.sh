6662768a771703774a849960e7f2be80a5b6601d
Rob Lawyer <37316368+lrobl@users.noreply.github.com>
2022-03-19 22:28:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@ echo "install lrobl vscode settings"
 echo "Copying dotfiles to: {$GITPOD_REPO_ROOT}"
 
 mv $HOME/.dotfiles/.vscode $GITPOD_REPO_ROOT/
-mv $HOME/.dotfiles/.eslintrc $GITPOD_REPO_ROOT/
